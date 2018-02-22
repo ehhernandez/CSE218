5117967916d8468dc8f27a0a1e77f7413ddaf3a0
Chen <chenb@sunysuffolk.edu>
2018-02-22 13:11:06 -0500

    Priority Queue and Simple Linked List

---
+++ Chen CSE218 L8 Linked List/src/simpleListedList/Demo.java
@@ -0,0 +1,20 @@
+package simpleListedList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		LinkList myList = new LinkList();
+		myList.insertFirst(1);
+		myList.insertFirst(2);
+		myList.insertFirst(3);
+		myList.insertFirst(4);
+		myList.displayList();
+//		myList.deleteFirst();
+//		myList.deleteFirst();
+//		myList.displayList();
+		
+		myList.delete(1);
+		myList.displayList();
+	}
+
+}
